66afa859f9001adb26d3ba370eb279761f8e3ee4
hongsieon <hongsieon@EE1C>
2026-03-05 18:00:38 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            textBox1 = new TextBox();
+            pictureBox1 = new PictureBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Control;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("맑은 고딕", 36F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            textBox1.Location = new Point(850, 96);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(424, 128);
+            textBox1.TabIndex = 3;
+            textBox1.Text = "홍시언";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(39, 116);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(640, 492);
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.Control;
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("맑은 고딕", 18F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            textBox2.ForeColor = SystemColors.MenuHighlight;
+            textBox2.Location = new Point(723, 291);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(694, 64);
+            textBox2.TabIndex = 5;
+            textBox2.Text = "수원대 컴퓨터SW 25학번";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.Control;
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Font = new Font("맑은 고딕", 13.875F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            textBox3.Location = new Point(799, 447);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(552, 50);
+            textBox3.TabIndex = 6;
+            textBox3.Text = "gorhanhee01@gmail.com";
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("맑은 고딕", 24F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button1.ForeColor = SystemColors.MenuHighlight;
+            button1.Location = new Point(782, 632);
+            button1.Name = "button1";
+            button1.Size = new Size(599, 136);
+            button1.TabIndex = 7;
+            button1.Text = "배경색　바꾸기";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(14F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1439, 826);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox textBox1;
+        private PictureBox pictureBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+    }
+}
